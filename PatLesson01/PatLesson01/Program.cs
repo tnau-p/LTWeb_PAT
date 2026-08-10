df9ea11e1263168a0ae5ea74a72616ead11e2ea8
Phạm Anh Tuấn <tnau06@gmail.com>
2026-08-10 21:31:23 +0700

    lesson01

---
+++ PatLesson01/PatLesson01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PatLesson01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ASP NET, hellooo");
+        }
+    }
+}
